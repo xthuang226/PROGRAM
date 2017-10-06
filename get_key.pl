b6e4bf09d768d9d82b0a592ef8b653ee4fdd3c6a
xthuang226 <xiaotaihuang@gmail.com>
2017-10-06 14:30:58 +0800

    20171006m2

--- get_key.pl
+++ get_key.pl
@@ -6,12 +6,7 @@ use feature 'say';
 use Math::Random qw(random_binomial random_uniform_integer);
 use LWP::UserAgent;
 
-
-
-open (my $OUTPUT2, ">log") || die $!;
-close $OUTPUT2;
-
-
+my $time = time;
 
 my @words = qw/a b c d e f g h i g k l m n o p q r s t u v w x y z A B C D E F G H I G K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9/;
 
@@ -20,11 +15,6 @@ foreach (@ARGV) {
 	my $id = $_;
 
 	my %unkey;
-
-	open ($OUTPUT2, ">>log") || die $!;
-	say $OUTPUT2 "===> $id";
-	close $OUTPUT2;
-	say "===> $id";
 
 	my $i = 1;
 	while () {
@@ -39,11 +29,6 @@ foreach (@ARGV) {
 			}
 
 			last unless defined $unkey{$key};
-
-			open ($OUTPUT2, ">>log") || die $!;
-			say $OUTPUT2 "\tGenerate untested key!";
-			close $OUTPUT2;
-			say "\tGenerate untested key!";
 		}
 
 
@@ -53,7 +38,7 @@ foreach (@ARGV) {
 
 		if ($status eq '200 OK') {
 
-			open (my $OUTPUT, ">>valid_url") || die $!;
+			open (my $OUTPUT, ">valid_url_".time) || die $!;
 			say $OUTPUT "Test times: $i";
 			say $OUTPUT "$url\n";
 			close $OUTPUT;
@@ -61,10 +46,9 @@ foreach (@ARGV) {
 			last;
 		}else{
 			$unkey{$key} = 1;
-			open ($OUTPUT2, ">>log") || die $!;
-			say $OUTPUT2 "\t$i\t$key\t$status\t".(56800235584 - scalar keys %unkey);
+			open (my $OUTPUT2, ">log_".$time) || die $!;
+			say $OUTPUT2 "\t$i\t$key\t$status\t$id\t".(56800235584 - scalar keys %unkey);
 			close $OUTPUT2;
-			say "\t$i\t$key\t$status\t".(56800235584 - scalar keys %unkey);			
 		}
 
 		$i++;
