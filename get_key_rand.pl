8b39d7ef52d5ddfd6e34868083f917bde93d007a
xthuang226 <xiaotaihuang@gmail.com>
2017-10-08 21:39:35 -0400

    20171009m2

--- get_key_rand.pl
+++ get_key_rand.pl
@@ -10,6 +10,8 @@ use LWP::UserAgent;
 my @words = qw/a b c d e f g h i g k l m n o p q r s t u v w x y z A B C D E F G H I G K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9/;
 
 
+my ($ua, $key, @keyc, $url, $response, $status, $OUTPUT, $OUTPUT2);
+
 foreach (@ARGV) {
 	my $id = $_;
 
@@ -18,28 +20,38 @@ foreach (@ARGV) {
 	my $i = 1;
 	while () {
 
-		my $key;
+                undef @keyc;
+	        @keyc = random_uniform_integer(6,0,61);
 
-		my @keyc = random_uniform_integer(6,0,61);
+                undef $key;
 
 		foreach (@keyc) {
 			$key .= $words[$_];
 		}
 
-		my $url = "https://sscn.afast.ws/$id/$key/main.m3u8";
+                undef $url;
+		$url = "https://sscn.afast.ws/$id/$key/main.m3u8";
 
-		my $status = &test_url($url);
+                undef $ua;
+                $ua = LWP::UserAgent->new;
+                $ua->agent('Mozilla/5.0');
+
+                undef $response;
+                $response = $ua->get( $url );
+
+                undef $status;
+                $status = $response->status_line;
 
 		if ($status eq '200 OK') {
 
-			open (my $OUTPUT, ">valid_url_".time) || die $!;
+		        open ($OUTPUT, ">valid_url_".time) || die $!;
 			say $OUTPUT "Test times: $i";
 			say $OUTPUT "$url\n";
 			close $OUTPUT;
 
-			last;
+			die;
 		}else{
-			open (my $OUTPUT2, ">log_".$time) || die $!;
+			open ($OUTPUT2, ">log_".$time) || die $!;
 			say $OUTPUT2 "\t$i\t$key\t$status\t$id\t".(56800235584 - $i);
 			close $OUTPUT2;
 		}
@@ -49,24 +61,4 @@ foreach (@ARGV) {
 
 }
 
-
-
-
-
-
-
-
-
-
-sub test_url {
-	my ($url) = @_;
-
-	my $ua = LWP::UserAgent->new;
-	$ua->agent('Mozilla/5.0');
-	$ua->cookie_jar({});
-
-	my $response = $ua->get( $url );
-
-	return $response->status_line;
-}
 
